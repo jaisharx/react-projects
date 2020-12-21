99976784a409be52104aad47f112c8338c186123
jai-sharma <sendtojsharma@gmail.com>
2020-12-21 21:28:08 +0530

    finished tours project - 02

---
+++ 02-tours-project/src/Loading.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Loading = () => {
+    return <div id="loading"></div>;
+};
+
+export default Loading;
---
+++ 02-tours-project/src/Tours.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Tour from './Tour';
+
+const Tours = ({ tours, removeTour }) => {
+    return (
+        <section>
+            <div className="title">
+                <h2>our tours</h2>
+                <div className="underline"></div>
+            </div>
+            <div>
+                {tours.map((tour) => (
+                    <Tour key={tour.id} removeTour={removeTour} {...tour} />
+                ))}
+            </div>
+        </section>
+    );
+};
+
+export default Tours;
