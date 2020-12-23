96d691adf3c507380457f30c9b2cda01950f38df
jai-sharma <sendtojsharma@gmail.com>
2020-12-23 12:15:35 +0530

    finished tabs project - 06

---
+++ 06-tabs-project/src/LoadingSpinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoadingSpinner() {
+    return (
+        <div className="loading-container">
+            <div id="loading"></div>
+        </div>
+    );
+}
+
+export default LoadingSpinner;
