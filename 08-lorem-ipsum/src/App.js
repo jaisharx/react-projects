25a168eb6a6a32959e90104fb836f7f93a68438b
jai-sharma <sendtojsharma@gmail.com>
2020-12-25 12:04:38 +0530

    finished lorem ipsum - 08

--- 08-lorem-ipsum/src/App.js
+++ 08-lorem-ipsum/src/App.js
@@ -2,5 +2,37 @@ import React, { useState } from 'react';
 import data from './data';
 
 export default function App() {
-    return <h2>lorem ipsum project setup</h2>;
+    const [count, setCount] = useState(0);
+    const [text, setText] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setText(data.slice(0, +count));
+    };
+
+    return (
+        <section className="section-center">
+            <h3>tired of boring lorem ipsum</h3>
+            <form className="lorem-form" onSubmit={handleSubmit}>
+                <label htmlFor="amount">paragraphs:</label>
+                <input
+                    min="0"
+                    max="8"
+                    type="number"
+                    name="amount"
+                    id="amount"
+                    value={count}
+                    onChange={(e) => setCount(+e.target.value)}
+                />
+                <button type="submit" className="btn">
+                    Generate
+                </button>
+            </form>
+            <article className="lorem-text">
+                {text.map((para, index) => (
+                    <p key={index}>{para}</p>
+                ))}
+            </article>
+        </section>
+    );
 }
