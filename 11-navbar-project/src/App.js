c2dba79e59f7ea98796b1392c92c5bf24c80602d
jai-sharma <sendtojsharma@gmail.com>
2020-12-27 08:21:38 +0530

    fixed fromatting

--- 11-navbar-project/src/App.js
+++ 11-navbar-project/src/App.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from 'react';
+import Navbar from './Navbar';
+
 function App() {
-  return (
-    <>
-      <h2>navbar project setup</h2>
-    </>
-  )
+    return (
+        <>
+            <h2>navbar project setup</h2>
+        </>
+    );
 }
 
-export default App
+export default App;
