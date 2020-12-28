c03b8a49357eaea9ab0b4c6322e8c1c742c04311
jai-sharma <sendtojsharma@gmail.com>
2020-12-28 10:16:30 +0530

    finished stripe submenus - 13

--- 13-stripe-submenus/src/context.js
+++ 13-stripe-submenus/src/context.js
@@ -1,2 +1,51 @@
 import React, { useState, useContext } from 'react';
 import sublinks from './data';
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+
+    const [location, setLocation] = useState({});
+    const [page, setPage] = useState({ page: '', links: [] });
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true);
+    };
+    const closeSidebar = () => {
+        setIsSidebarOpen(false);
+    };
+
+    const openSubmenu = (text, coordinates) => {
+        const page = sublinks.find((link) => link.page === text.toLowerCase());
+        setPage(page);
+        
+        setLocation(coordinates);
+        setIsSubmenuOpen(true);
+    };
+    const closeSubmenu = () => {
+        setIsSubmenuOpen(false);
+    };
+
+    return (
+        <AppContext.Provider
+            value={{
+                page,
+                location,
+                isSidebarOpen,
+                isSubmenuOpen,
+                openSidebar,
+                closeSidebar,
+                openSubmenu,
+                closeSubmenu,
+            }}
+        >
+            {children}
+        </AppContext.Provider>
+    );
+};
+
+export const useGloablContext = () => {
+    return useContext(AppContext);
+};
--- 13-stripe-submenus/src/index.js
+++ 13-stripe-submenus/src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { AppProvider } from './context';
+
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <AppProvider>
+            <App />
+        </AppProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
